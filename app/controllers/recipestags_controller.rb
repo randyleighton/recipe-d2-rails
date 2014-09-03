7240823e84960e5099db806a02fe9aa0eb5abf42
Jeremy Whitaker & Randy Leighton <student@epicodus.com>
2014-09-03 12:08:54 -0700

    add ability to delete tag from recipe

--- app/controllers/recipestags_controller.rb
+++ app/controllers/recipestags_controller.rb
@@ -1,10 +1,19 @@
 class RecipestagsController < ApplicationController
 
   def create
-    @recipe = Recipe.find(params[:id])
-    @tag = Tag.find(params[:tag])
-    @recipe.tags << @tag
-    flash[:notice] = "Updated Successfully."
-    redirect_to("/recipes/#{@recipe.id}")
-  end
+  @recipe = Recipe.find(params[:id])
+  @tag = Tag.find(params[:tag])
+  @recipe.tags << @tag
+  flash[:notice] = "Updated Successfully."
+  redirect_to("/recipes/#{@recipe.id}")
+end
+
+def delete
+  @tag = Tag.find(params[:tag_id])
+  @recipe = Recipe.find(params[:id])
+  @recipe.tags.delete(@tag)
+  flash[:notice] = "Removed Successfully."
+  redirect_to("/recipes/#{@recipe.id}")
+end
+
 end
